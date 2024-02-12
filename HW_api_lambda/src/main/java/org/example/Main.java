753d23deeac13a55797a4a5d9a112b19083694d9
fanatbostona <stasons.mail@gmail.com>
2024-02-12 13:07:50 +0300

    first iteration
    some problems with Double::max

---
+++ HW_api_lambda/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main
+{
+
+
+    public static void main(String[] args) {
+        Function<String,Double> stringToLength = s -> {
+            Pattern p = Pattern.compile("\\d+");
+            Matcher m = p.matcher(s);
+            ArrayList<Integer> numbers = new ArrayList<>();
+            while(m.find()) {
+                numbers.add(Integer.valueOf(m.group()));
+            }
+            return Math.sqrt(Math.pow((numbers.get(2)-numbers.get(0)),2) - Math.pow((numbers.get(3)-numbers.get(1)),2));
+        };
+        try
+        {
+            Optional <Integer> max = new BufferedReader(new FileReader("D:/Desktop/lines.txt")).lines()
+                    .map(stringToLength)
+                    .max(Double::max);
+            System.out.println(max);
+
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
