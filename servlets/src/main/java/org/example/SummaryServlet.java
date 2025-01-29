68ccc6b31f8475d96e80db862ba1f7e59b016f39
Stanislav <stasons.mail@gmail.com>
2025-01-29 15:03:58 +0300

    hw servlets

---
+++ servlets/src/main/java/org/example/SummaryServlet.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SummaryServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
